68ac238d22a241fe84ec866048912c5421cd5e2b
Mohamed Amine Garci <mohamed.garci@drivequant.com>
2020-11-18 19:20:34 +0100

    Add camera feature and display picture into an imageView.

--- app/src/main/java/com/example/discovery/AddCountryActivity.java
+++ app/src/main/java/com/example/discovery/AddCountryActivity.java
@@ -1,12 +1,23 @@
 package com.example.discovery;
 
+import android.Manifest;
+import android.app.Activity;
 import android.content.Intent;
 
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.os.Build;
 import android.os.Bundle;
 
+import android.provider.MediaStore;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.discovery.DB.SQLiteOperations;
@@ -16,10 +27,13 @@ import static com.example.discovery.MainActivity.DB_ACTION;
 
 public class AddCountryActivity extends AppCompatActivity {
     public static final String COUNTRY_INTENT_EXTRA = "COUNTRY";
+    private static final int CAMERA_REQUEST = 1888;
+    private static final int MY_CAMERA_PERMISSION_CODE = 100;
 
     private EditText etCountryName;
     private EditText etCountryCapital;
     private Button btnAddUpdateCountry;
+    private ImageView countryFlag;
 
     public CrudMethod crudMethod;
     private SQLiteOperations sqliteOperations;
@@ -33,6 +47,21 @@ public class AddCountryActivity extends AppCompatActivity {
         etCountryName = findViewById(R.id.country_name);
         etCountryCapital = findViewById(R.id.country_capital);
         btnAddUpdateCountry = findViewById(R.id.btn_add_country);
+        countryFlag = findViewById(R.id.country_flag_get);
+
+
+        countryFlag.setOnClickListener(v -> {
+
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                if (checkSelfPermission(Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+                    requestPermissions(new String[]{Manifest.permission.CAMERA}, MY_CAMERA_PERMISSION_CODE);
+                } else {
+                    Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                    startActivityForResult(cameraIntent, CAMERA_REQUEST);
+                }
+            }
+        });
+
 
         sqliteOperations = new SQLiteOperations(this);
 
@@ -107,6 +136,29 @@ public class AddCountryActivity extends AppCompatActivity {
             intent.putExtra("COUNTRY_ID", String.valueOf(CountryItemId));
             setResult(RESULT_OK, intent);
             finish();
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == MY_CAMERA_PERMISSION_CODE) {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                Toast.makeText(this, "camera permission granted", Toast.LENGTH_LONG).show();
+                Intent cameraIntent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(cameraIntent, CAMERA_REQUEST);
+            } else {
+                Toast.makeText(this, "camera permission denied", Toast.LENGTH_LONG).show();
+            }
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == CAMERA_REQUEST && resultCode == Activity.RESULT_OK) {
+            Bitmap photo = (Bitmap) data.getExtras().get("data");
+            countryFlag.setImageBitmap(photo);
         }
     }
 }
